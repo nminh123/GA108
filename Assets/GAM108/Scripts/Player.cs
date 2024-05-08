04a3eb6c0ed8fd1fc7855f0fdb61f25a41a623c7
nminh123 <caonhatminh245@gmail.com>
2024-05-08 23:45:43 +0700

    Đang làm dở Double Jump

--- Assets/GAM108/Scripts/Player.cs
+++ Assets/GAM108/Scripts/Player.cs
@@ -5,17 +5,68 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    [SerializeField]
-    private float speed;
-    private float JumpingPower;
+    private float moveDirection;
+    [SerializeField] private int countJump = 0;
+    [SerializeField] private float speed;
+    [SerializeField] private float JumpForce; // jump force
     private bool isFacingRight = true;
-    private bool isGround;
+    [SerializeField] private bool isGround;
+    private bool DoubleJump;
+    private Rigidbody2D rigid;
     public void Start()
     {
-
+        rigid = GetComponent<Rigidbody2D>();
     }
     public void Update()
     {
+        Move();
+        if(Input.GetKey(KeyCode.Space))
+        {
+            if(isGround)
+            {
+                Jump();
+                isGround = false;
+                DoubleJump = false;
+            }
+            else if (DoubleJump)
+            {
+                Jump();
+                DoubleJump = false; 
+            }
+        }
         
+        if(isGround && !Input.GetKey(KeyCode.Space))
+        {
+            DoubleJump = false;
+        }
+    }
+    
+    public void Move()
+    {
+        moveDirection = Input.GetAxis("Horizontal");
+        rigid.velocity = new Vector2(moveDirection * speed, rigid.velocity.y);
+        Flip();
+
+    }
+    public void Flip()
+    {
+        isFacingRight = !isFacingRight;
+        Vector3 localScale = transform.localScale;
+        localScale.x *= -1f;
+        transform.localScale = localScale;
+    }
+
+    public void Jump()
+    {
+        rigid.velocity = new Vector2(rigid.velocity.x, JumpForce);
+        DoubleJump = !DoubleJump;
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.gameObject.tag == "Ground")
+        {
+            isGround = true;
+        }
     }
 }
