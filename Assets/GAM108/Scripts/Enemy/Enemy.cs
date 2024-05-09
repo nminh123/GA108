07e2fd681ae5a8a8826dbbe6a45e751b13d36fa8
Minh280123 <127473811+Minh280123@users.noreply.github.com>
2024-05-09 13:45:36 +0700

    Enemy Scripts
    Sửa lại lỗi di chuyển trái phải cho enemy

--- Assets/GAM108/Scripts/Enemy/Enemy.cs
+++ Assets/GAM108/Scripts/Enemy/Enemy.cs
@@ -1,30 +1,78 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
+    #region toc do quai
+    //toc do cua quai
     [SerializeField]
-    private int speed;
+    private float speed;
+    #endregion
+    #region toa do cho quai di chuyen trai phai
+    //toa do cho quai di chuyen trai phai
+    [SerializeField]
+    private float leftEnemy = 0f;
+    [SerializeField]
+    private float rightEnemy = 0f;
+    #endregion
 
-    private bool 
+    //check xem quai di chuyen trai hay phai
+    private bool isMovingRight = true;
 
+    private SpriteRenderer eScale;
     private Animator eAnimator;
+
     // Start is called before the first frame update
     void Start()
     {
+        eScale = GetComponent<SpriteRenderer>();
         eAnimator = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        MoveEnemy();        
+        MoveEnemy();
     }
 
     private void MoveEnemy()
     {
-        var direction = Vector3.left;
+        //lay vi tri hien tai cua enemy
+        var currentPosition = transform.localPosition;
+        if (currentPosition.x > rightEnemy)
+        {
+            //di chuyen sang trai
+            isMovingRight = false;
+            eScale.flipX = true;
+        }
+        else if(currentPosition.x < leftEnemy)
+        {   //di chuyen sang phai
+            isMovingRight = true;
+            eScale.flipX = false;
+        }
+        var direction = isMovingRight ? Vector3.right : Vector3.left;
         transform.Translate(direction * speed * Time.deltaTime);
     }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            //StartCoroutine(eDieAnimation());
+            Destroy(gameObject);
+        }
+    }
+
+    //test
+    private IEnumerator eDieAnimation()
+    {
+        eAnimator.SetBool("isDie", true);
+        yield return new WaitForSeconds(2f);
+        Destroy(gameObject);
+
+
+    }
+
 }
