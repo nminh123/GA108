1ad382194deb7b62317766aa043233172e1c928d
Fjnnn2907 <phuongphipho2005@gmail.com>
2024-05-29 11:45:50 +0700

    Fix coin

--- Assets/GAM108/Scripts/Item/Coin.cs
+++ Assets/GAM108/Scripts/Item/Coin.cs
@@ -6,20 +6,16 @@ public class Coin : MonoBehaviour
 {
     public int Point;
     public Score score;
+    
+    private bool isCoined = false;
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if(collision.tag == Tag.Playertag)
+        if(collision.tag == Tag.Playertag && !isCoined)
         {
+            isCoined = true;
             score.addScore(Point);
             Destroy(this.gameObject);
         }
     }
-    private void OnCollisionEnter2D(Collision2D collision)
-    {
-        if (collision.gameObject.tag == Tag.Playertag)
-        {
-            score.addScore(Point);
-            Destroy(this.gameObject);
-        }
-    }
+
 }
