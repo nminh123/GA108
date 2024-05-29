8514795c98cecafe0e4c3ab1bad1ab74b8529748
Fjnnn2907 <phuongphipho2005@gmail.com>
2024-05-29 09:55:21 +0700

    Load Sence
    Fix vi tri enemy

---
+++ Assets/GAM108/Scripts/NextLevel.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class NextLevel : MonoBehaviour
+{
+    public Animator anim;
+    public GameObject _LoadSecne;
+    private void Awake()
+    {
+        _LoadSecne.SetActive(true);
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.tag == Tag.Playertag)
+        {
+            StartCoroutine(LoadSecne());
+            
+        }
+    }
+    IEnumerator LoadSecne()
+    {
+        anim.SetTrigger("end");
+        yield return new WaitForSecondsRealtime(1);
+        anim.SetTrigger("start");
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+}
